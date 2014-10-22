23b4c3648aa19d9a38748cb99956667daa1f155d
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2014-10-22 16:53:38 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/**
+*
+**/
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+	      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+	      options: {
+	      	node: true,
+	        // options here to override JSHint defaults
+	        globals: {
+	          jQuery: true,
+	          console: true,
+	          module: true,
+	          document: true
+	        }
+	      }
+	    },
+	     simplemocha: {
+            options: {
+                globals: ['expect'],
+                timeout: 3000,
+                ignoreLeaks: true,
+                ui: 'bdd',
+                reporter: 'tap'
+            },
+            all: { src: ['test/*spec.js'] }
+        }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-simple-mocha');
+	grunt.registerTask('default', ['jshint', 'simplemocha']);
+};
---
+++ test/server.spec.js
@@ -0,0 +1,12 @@
+/**
+**/
+process.env.NODE_ENV = 'test';
+
+var should = require('should');
+var app = require('../src/backend');
+
+describe("Websockets Server", function() {
+	it("Should broadcast a new movement event", function(done) {
+		false.should.be.true("not implemented.");
+	});
+});
